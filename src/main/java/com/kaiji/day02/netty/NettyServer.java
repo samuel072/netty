e56bc0e8cdb03a047a2454119c15dd6d16c159ce
开机 <han_wuhan@sina.com>
2018-10-24 21:20:19 +0800

    netty服务器端

--- src/main/java/com/kaiji/day02/netty/NettyServer.java
+++ src/main/java/com/kaiji/day02/netty/NettyServer.java
@@ -1,5 +1,20 @@
 package com.kaiji.day02.netty;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
 public class NettyServer {
 
+	public static void main(String[] args) {
+		// 第一步: 创建服务器类
+		ServerBootstrap bootstrap = new ServerBootstrap();
+		// 第二步: 创建nioScoket工厂
+		ExecutorService bossExecutor = Executors.newCachedThreadPool();
+		ExecutorService workerExecutor = Executors.newCachedThreadPool();
+		bootstrap.setFactory(new NioServerSocketChannelFactory(bossExecutor, workerExecutor));
+		// 第三步: 创建管道工厂
+	}
 }
